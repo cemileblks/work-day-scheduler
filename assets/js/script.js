0f7446f09abac552bc14e4b30a5733a19ee19139
Cemileb <cemilebalkas@gmail.com>
2024-01-02 14:41:51 +0000

    added code to color code present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 let currentDay = $('#currentDay');
-
+let saveBtn = $('.saveBtn');
+let currentHour = "hour-" + dayjs().format('H');
 
 
 
 currentDay.text(dayjs().format('dddd, MMMM DD'));
+
+console.log (currentHour);
+
+
+$('.time-block').each(function(){
+    let timeBlock = $(this).attr('id');
+    console.log(timeBlock);
+
+    if (timeBlock === currentHour) {
+        console.log(true);
+        $(this).addClass("present");
+    } else if (timeBlock > currentHour) {
+        $(this).addClass("future");
+    }
+});
