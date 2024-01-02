a40a6a5713d76a5546bd73398d029ecf1df0658b
Cemileb <cemilebalkas@gmail.com>
2024-01-02 16:35:42 +0000

    added save local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let currentDay = $('#currentDay');
 let saveBtn = $('.saveBtn');
 let currentHour = parseInt(dayjs().format('H'));
-
+let hourDescriptions = JSON.parse(localStorage.getItem('hourDescriptions')) || {};
 
 
 currentDay.text(dayjs().format('dddd, MMMM DD'));
@@ -20,3 +20,39 @@ $('.time-block').each(function(){
         $(this).addClass("future");
     }
 });
+
+// to save the data to local storage on save button click
+saveBtn.on("click",function(event) {
+    // console.log(event);
+    let timeBlock = $(event.target).closest('.time-block');
+    // console.log(timeBlock);
+    let timeBlockId = timeBlock.attr('id');
+    // console.log(timeBlockId);
+
+    let textContent = timeBlock.find('.description').val();
+    console.log(textContent);
+
+    hourDescriptions[timeBlockId] = textContent;
+    console.log(hourDescriptions);
+
+    localStorage.setItem('hourDescriptions', JSON.stringify(hourDescriptions));
+});
+
+
+// to load data from local storage when page loads
+$('.time-block').each(function () {
+    let timeBlockId = $(this).attr('id');
+    let savedContent = localStorage.getItem('hourDescriptions');
+
+    if (savedContent !== null) {
+      // to get the original object
+      hourDescriptions = JSON.parse(savedContent);
+      // update the text area with saved content
+      let descriptionOfTimeBlock = hourDescriptions[timeBlockId];
+
+      if (descriptionOfTimeBlock !== undefined) {
+        console.log(this);
+        $(this).find('.description').val(descriptionOfTimeBlock);
+      }
+    }
+  });
